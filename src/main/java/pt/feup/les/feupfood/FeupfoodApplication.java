a80f5216faef582193d5689d2594ba5f56a6e556
Orlando-pt <orlando.macedo15@ua.pt>
2021-11-17 16:29:13 +0000

    update repo with spring boot files

---
+++ src/main/java/pt/feup/les/feupfood/FeupfoodApplication.java
@@ -0,0 +1,13 @@
+package pt.feup.les.feupfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeupfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeupfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/feup/les/feupfood/FeupfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.feup.les.feupfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeupfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
