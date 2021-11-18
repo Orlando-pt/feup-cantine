ff0832eb81afed04a9979b9a9a0b2a0ae536adc2
Orlando-pt <orlando.macedo15@ua.pt>
2021-11-18 11:43:17 +0000

    separate ut and it

---
+++ src/test/java/pt/feup/les/feupfood/NotTestThisTestIT.java
@@ -0,0 +1,13 @@
+package pt.feup.les.feupfood;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class NotTestThisTestIT {
+
+    @Test
+    void eraseTest() {
+        assertTrue(true);
+    }    
+}
